3aebd83d574b674e88dc43115089c328bfd4a52b
Alistair Michael <alistair.michael@uq.edu.au>
2025-01-08 12:30:57 +1000

    add cache and threading to server

--- bin/main.ml
+++ bin/main.ml
@@ -1,91 +1,154 @@
 open LibASL
-
 open Cohttp
-open Cohttp_lwt_unix
+open Cohttp_eio
 open List
 open Asl_ast
-open Lwt
 
+let persistent_env = Option.get (Arm_env.aarch64_evaluation_environment ())
+let count = Atomic.make 0
 
-let persistent_env = lazy (Option.get (Arm_env.aarch64_evaluation_environment ()))
+let eval_instr (opcode : string) : string * string =
+  let pp_raw stmt : string =
+    Utils.to_string (Asl_parser_pp.pp_raw_stmt stmt) |> String.trim
+  in
+  let _address = None in
 
-let eval_instr (opcode: string) : string * string =
-    let pp_raw stmt : string = Utils.to_string (Asl_parser_pp.pp_raw_stmt stmt) |> String.trim  in
-    let _address = None in
+  let env' = persistent_env in
+  let lenv = Dis.build_env env' in
+  let decoder = Eval.Env.getDecoder env' (Ident "A64") in
+  let enc, stmts =
+    Dis.dis_decode_entry_with_inst env' lenv decoder (Z.of_string opcode)
+  in
 
-    let env' = Lazy.force persistent_env in
-    let lenv = Dis.build_env env' in
-    let decoder = Eval.Env.getDecoder env' (Ident "A64") in
-    let (enc,stmts) = Dis.dis_decode_entry_with_inst env' lenv decoder (Z.of_string opcode) in
+  let stmts' = List.map pp_raw stmts in
+  (enc, String.concat "\n" stmts')
 
-    let stmts'   = List.map pp_raw stmts in
-    enc, String.concat "\n" stmts'
-
-let get_reply (jsonin: string) : Cohttp.Code.status_code * string =
-  (*let json  = Yojson.Safe.from_string jsonin in *)
+let get_reply (jsonin : string) : Cohttp.Code.status_code * string =
   let make_reply code tail =
-    (code, Yojson.Safe.to_string (`Assoc (["instruction", `String jsonin] @ tail))) in
-  Printf.printf "Disassembling '%s'\n" jsonin;
-  flush stdout;
-  match (eval_instr jsonin) with
-  | exception e -> make_reply `Internal_server_error ["error", `String (Printexc.to_string e)]
-  | enc, x -> make_reply `OK [ "encoding", `String enc; "semantics", `String x; ]
-
+    ( code,
+      Yojson.Safe.to_string
+        (`Assoc ([ ("instruction", `String jsonin) ] @ tail)) )
+  in
+  match eval_instr jsonin with
+  | exception e ->
+      make_reply `Internal_server_error
+        [ ("error", `String (Printexc.to_string e)) ]
+  | enc, x ->
+      make_reply `OK [ ("encoding", `String enc); ("semantics", `String x) ]
 
 let unsupp_method_resp : Cohttp.Code.status_code * string =
-  (`Method_not_allowed, Yojson.Safe.to_string (`Assoc [("error", `String "unsupported method.")]))
+  ( `Method_not_allowed,
+    Yojson.Safe.to_string (`Assoc [ ("error", `String "unsupported method.") ])
+  )
 
 let missing_param : Cohttp.Code.status_code * string =
-  (`Bad_request, Yojson.Safe.to_string (`Assoc [("error", `String "missing opcode param.")]))
+  ( `Bad_request,
+    Yojson.Safe.to_string
+      (`Assoc [ ("error", `String "missing opcode param.") ]) )
 
 let try_set_flags xs : (unit, Cohttp.Code.status_code * string) Result.t =
-  match (List.iter Flags.set_flag xs) with
-  | exception (Arg.Bad _ as e) -> Result.error (`Bad_request, Yojson.Safe.to_string (`Assoc [("error", `String (Printexc.to_string e))]))
+  match List.iter Flags.set_flag xs with
+  | exception (Arg.Bad _ as e) ->
+      Result.error
+        ( `Bad_request,
+          Yojson.Safe.to_string
+            (`Assoc [ ("error", `String (Printexc.to_string e)) ]) )
   | _ -> Result.ok ()
 
-let get_resp (opcode: string) : Cohttp.Code.status_code * string =
-    get_reply opcode
+let get_resp (opcode : string) : Cohttp.Code.status_code * string =
+  get_reply opcode
 
-let server addr port =
+let mutex = Eio.Mutex.create ()
+
+type 'a work = Decode of (string * ('a, exn) result Eio.Promise.u)
+
+let has_work = Eio.Condition.create ()
+
+module DisCache = Lru_cache.Make (Core.String)
+
+let cache : (Code.status_code * string) DisCache.t =
+  DisCache.create ~max_size:5000 ()
+
+let cache_mutex = Eio.Mutex.create ()
+
+let submit_req_executor_pool pool opcode =
+  (* CACHE IS NOT THREADSAFE, only call this from one domain *)
+  let default () =
+    Atomic.incr count;
+    Eio.Executor_pool.submit_exn pool ~weight:0.001 (fun () -> get_resp opcode)
+  in
+  DisCache.find_or_add cache opcode ~default
+
+let server socket addr port get_resp =
   Printf.printf "Started aslp-server at http://%s:%d\n" addr port;
   flush stdout;
 
   let oldflags = Flags.get_flags () in
 
-  let callback _conn req body =
+  let callback _conn req (body : Server.body) =
     let uri = req |> Request.uri in
     let _meth = req |> Request.meth |> Code.string_of_method in
     let _headers = req |> Request.headers |> Header.to_string in
-    let body' = body |> Cohttp_lwt.Body.to_string in
 
     Flags.set_flags oldflags;
 
-    let resp' = 
-      match (Option.map try_set_flags (Uri.get_query_param' uri "flags")) with
-      | Some (Error xs) -> Lwt.return xs
-      | Some (Ok ()) | None ->
-        match (Request.meth req, Uri.get_query_param uri "opcode") with
-        | `POST, _ -> body' >|= get_resp
-        | `GET, Some param -> Lwt.return (get_resp param)
-        | `GET, None -> Lwt.return missing_param
-        |  _ -> Lwt.return unsupp_method_resp
+    let code, body =
+      match Option.map try_set_flags (Uri.get_query_param' uri "flags") with
+      | Some (Error xs) -> xs
+      | Some (Ok ()) | None -> (
+          match (Request.meth req, Uri.get_query_param uri "opcode") with
+          | `POST, _ ->
+              let body' : string = Eio.Flow.read_all body in
+              get_resp body'
+          | `GET, Some param -> get_resp param
+          | `GET, None -> missing_param
+          | _ -> unsupp_method_resp)
     in
-    resp' >>= fun (code, body) -> Server.respond_string ~status:code ~body ()
+    Server.respond_string ~status:code ~body ()
   in
-  Server.create ~mode:(`TCP (`Port port)) (Server.make ~callback ())
+  Server.run
+    ~mode:(`TCP (`Port port))
+    socket (Server.make ~callback ())
+    ~on_error:(Eio.traceln "Error handling connection: %a" Fmt.exn)
 
 let port_opt : int ref = ref 8000
 let addr_opt : string ref = ref "127.0.0.1"
+let threads_opt : int ref = ref 2
 
 let speclist =
   [
-    ("--host", Arg.Set_string addr_opt, "Server address (default loopback)");
+    ("--host", Arg.Set_string addr_opt, "Server ip address (default 127.0.0.1)");
     ("--port", Arg.Set_int port_opt, "Server port (default 8000)");
+    ("--threads", Arg.Set_int threads_opt, "Number of lifter worker threads");
   ]
 
-let () = Arg.parse speclist ignore "usage: aslp-server --host HOSTNAME --port PORT"
+let rec periodic t () =
+  Eio.Time.sleep t 5.0;
+  Eio.traceln "Cache hit rate: %f handled: %d" (DisCache.hit_rate cache)
+    (Atomic.get count);
+  periodic t ()
 
 let () =
-  let _address = Unix.(ADDR_INET (inet_addr_of_string !addr_opt, !port_opt)) in
-  Lwt_main.run (server !addr_opt !port_opt)
-
+  Arg.parse speclist ignore "usage: aslp-server --host HOSTNAME --port PORT";
+  let main env =
+    let unix_addr = Unix.inet_addr_of_string !addr_opt in
+    let e = Eio_unix.Net.Ipaddr.of_unix unix_addr in
+    let addr = `Tcp (e, !port_opt) in
+    let work sw =
+      Eio.Fiber.fork ~sw (periodic env#clock);
+      let pool =
+        Eio.Executor_pool.create ~sw env#domain_mgr ~domain_count:!threads_opt
+      in
+      let decode opcode = submit_req_executor_pool pool opcode in
+      let socket =
+        Eio.Net.listen ~sw ~backlog:5 ~reuse_addr:true env#net addr
+      in
+      let fd = Eio_unix.Net.fd socket in
+      Eio_unix.Fd.use fd
+        (fun fd -> Unix.setsockopt fd Unix.TCP_NODELAY true)
+        ~if_closed:(fun () -> ());
+      server socket !addr_opt !port_opt decode
+    in
+    Eio.Switch.run work
+  in
+  Eio_main.run main
