268fdddecc8ce810f06137332b0e044cd6c0f68f
Alistair Michael <alistair.michael@uq.edu.au>
2025-01-16 18:17:07 +1000

    expose aslp_common, get stats, format

--- aslp-client-server-marshallrpc/lib/client.ml
+++ aslp-client-server-marshallrpc/lib/client.ml
@@ -1,52 +1,52 @@
-
 open Aslp_common.Common
-
 open Lwt
 
-let connection = lazy ( Lwt_io.open_connection @@ Rpc.sockaddr ())
-
+let connection = lazy (Lwt_io.open_connection @@ Rpc.sockaddr ())
 let set_addr ~filename = Rpc.set_addr filename
 
-let cin () = let* (ic,_) = Lazy.force connection in 
-    if (Lwt_io.is_closed ic) then (failwith "connection (in) closed") ;
-    return ic
-let cout () = let* (_,oc) = Lazy.force connection in 
-    if (Lwt_io.is_closed oc) then (failwith "connection (out) closed") ;
-    return oc
+let cin () =
+  let* ic, _ = Lazy.force connection in
+  if Lwt_io.is_closed ic then failwith "connection (in) closed";
+  return ic
 
-let shutdown_server () = 
+let cout () =
+  let* _, oc = Lazy.force connection in
+  if Lwt_io.is_closed oc then failwith "connection (out) closed";
+  return oc
+
+let shutdown_server () =
   let* cout = cout () in
   let m : Rpc.msg_call = Shutdown in
   Lwt_io.write_value cout m
 
-let lift ~(opcode_be: string) (addr : int) = 
-  let* cout = cout()  in
-  let* cin = cin() in
-  let cm : Rpc.msg_call = Lift {opcode_be; addr} in
-  let*() = Lwt_io.write_value cout cm in
-  let* resp : Rpc.msg_resp = Lwt_io.read_value cin  in
-  match resp with 
-    | Ok x -> return x
-    | All _ -> Lwt.fail_with "did not expect multi response"
+let lift ~(opcode_be : string) (addr : int) =
+  let* cout = cout () in
+  let* cin = cin () in
+  let cm : Rpc.msg_call = Lift { opcode_be; addr } in
+  let* () = Lwt_io.write_value cout cm in
+  let* resp : Rpc.msg_resp = Lwt_io.read_value cin in
+  match resp with
+  | Ok x -> return x
+  | All _ -> Lwt.fail_with "did not expect multi response"
 
-let lift_one ~(opcode_be: string) (addr : int) = 
+let lift_one ~(opcode_be : string) (addr : int) =
   Lwt_main.run (lift ~opcode_be addr)
 
-let lift_multi ~(opcodes: (string * int) list) : opcode_sem list Lwt.t = 
-  let* lift_m = 
-    let* cout = cout() in let* cin = cin() in
+let lift_multi ~(opcodes : (string * int) list) : opcode_sem list Lwt.t =
+  let* lift_m =
+    let* cout = cout () in
+    let* cin = cin () in
     let cm : Rpc.msg_call = LiftAll opcodes in
-    let*() = Lwt_io.write_value cout cm in
-    let* resp : Rpc.msg_resp = Lwt_io.read_value cin  in
-    match resp with 
-      | All x -> return x
-      | Ok x -> return [x]
-    in 
-  let* _ = Lwt_list.iter_s (function 
-    | Ok _ ->  Lwt.return ()
-    | Error ({opcode; error}) -> (
-      Lwt_io.printf "Lift error : %s :: %s\n"  opcode error;
-    )
-    ) lift_m
+    let* () = Lwt_io.write_value cout cm in
+    let* resp : Rpc.msg_resp = Lwt_io.read_value cin in
+    match resp with All x -> return x | Ok x -> return [ x ]
+  in
+  let* _ =
+    Lwt_list.iter_s
+      (function
+        | Ok _ -> Lwt.return ()
+        | Error { opcode; error } ->
+            Lwt_io.printf "Lift error : %s :: %s\n" opcode error)
+      lift_m
   in
   return lift_m
--- aslp-client-server-marshallrpc/lib/server.mli
+++ aslp-client-server-marshallrpc/lib/server.mli
@@ -1,23 +1,22 @@
-
 open Aslp_common.Common
 
-
-(**
-   Set the address (unix socket filename) to connect to (must be called before first call to lift)
-   Overrides the environment variable.
-*)
 val set_addr : filename:string -> unit
+(** Set the address (unix socket filename) to connect to (must be called before
+    first call to lift) Overrides the environment variable. *)
 
-
-(**
-   Start the server listening on the unix domain socket filename supplied by 
-   either a call to [set_addr] or environment variable [GTIRB_SEM_SOCKET], or default file [./aslp_rpc_socket].
-   
-   (will not return until server is shutdown)
-*)
 val start_server : unit -> unit
+(** Start the server listening on the unix domain socket filename supplied by
+    either a call to [set_addr] or environment variable [GTIRB_SEM_SOCKET], or
+    default file [./aslp_rpc_socket].
+
+    (will not return until server is shutdown) *)
+
+val lift_opcode : ?cache:bool -> opcode_be:string -> int -> opcode_sem
+(** Invoke the lifter directly to lift an opcode *)
+
+type stats = { success : int; fail : int; total : int; cache_hit_rate : float }
 
 (**
-  Invoke the lifter directly to lift an opcode
+   Return statistics on the number of opcodes lifted through lift_opcode.
 *)
-val lift_opcode : ?cache:bool -> opcode_be:string -> int -> opcode_sem
+val get_local_lifter_stats : unit -> stats
